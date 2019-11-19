abd62d12eaf33a9918481570a3eb3dc4c171767a
Victor Lelis <37216112+victorlelissoares@users.noreply.github.com>
2019-11-19 10:13:34 -0400

    Update main_ldecc.c

--- main_ldecc.c
+++ main_ldecc.c
@@ -1,5 +1,5 @@
 #include "listadecc.h"
-//LEIA OS COMENTÁRIOS E APAGUE-OS
+
 int main (int argc, char *argv[]) {
 	/*esse atoi é pra converter o número de arquivos 
 	que é passado por linha de comando para inteiro*/
@@ -8,9 +8,9 @@ int main (int argc, char *argv[]) {
 	/*I controlará a abertura de arquivos e , j andará no vetor da linha de comando, 
 	atribuindo o nome de cada aquivo para a função de abertura*/
 	int i=0, j = 2;
-	no *p;
-	FILE *arquivos[argc-2];
-	char palavra[MAX];
+	no *p, *q;
+	FILE *arquivos[num_arquivos];
+	char nova_chave[MAX];
 	FILE *saida;
 	ldecc *lista;
 
@@ -18,6 +18,8 @@ int main (int argc, char *argv[]) {
 
 	/*laço para abertura de arquivos através da linha de comando*/
 	while(i < num_arquivos){
+		/*caso na tentativa de abrir um arquivo returne null, quer dizer que não houve sucesso, 
+		então o programa será encerrado sem processar nada*/
 		if((arquivos[i] = fopen(argv[j], "r")) == NULL){
 			printf("Entrada inválida!\n");
 			return 0;
@@ -25,18 +27,24 @@ int main (int argc, char *argv[]) {
 		i++;
 		j++;
 	}
-	//após isso, terá aberto todos os arquvos ou, terá encerrado
+	/*após isso, terá aberto todos os arquvos ou terá encerrado*/
 
-
+	j=2;
 	for(i=0; i < num_arquivos; i++){
-		while(!feof(arquivos[i])){
-			fscanf(arquivos[i], "%29[A-Za-zÀ-ú]%*[^A-Za-zÀ-ú]", palavra);
-			cnvr_lw(palavra);
-			insere_ordenado_ldecc(lista, palavra, num_arquivos, i);
+		/*caso o arquivo exista e nao contenha nada,getsize que retorna o tamanho do arquivo, retornará 0 e nem entrara no while. */
+		if(get_size(argv[j++]) != 0){
+			/*enquanto não chegar ao fim do arquivo, o percorrerá procurando cada string 
+			e alocando de forma ordenada na ldecc*/
+			while(!feof(arquivos[i])){
+				fscanf(arquivos[i], "%29[A-Za-zÀ-ú]%*[^A-Za-zÀ-ú]", nova_chave);
+				insere_ordenado_ldecc(lista, converte_lw(nova_chave), num_arquivos, i);			
+			}	
 		}
+		/*após processar todas as strings de um arquivo, o mesmo será fechado*/
+		fclose(arquivos[i]);
 	}
 
-	saida = fopen("meuresultado.out", "w+");
+	saida = fopen("resultado.out", "w+");
 
 	for(p=lista->cabeca->prox; p != NULL; p = p->prox){
 		fprintf(saida,"%s", p->chave);
@@ -45,6 +53,14 @@ int main (int argc, char *argv[]) {
 		}
 		fprintf(saida, "\n");
 	}
+
+	p=lista->cabeca->prox;
+	while(p!=NULL){
+		q = p->prox;
+		free(p);
+		p=q;
+	}
+	free(lista);
 
 	return 0;
 }
